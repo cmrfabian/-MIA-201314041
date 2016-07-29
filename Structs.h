df6fdd1e0354767652d888a2f680e65d433e1675
Christian <chrisfabian594@gmail.com>
2016-07-28 18:03:22 -0600

    Commit Inicial

---
+++ Structs.h
@@ -0,0 +1,30 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+typedef struct Particiones{
+    char part_status;
+    char part_type;
+    char part_fit;
+    int part_start;
+    int part_size;
+    char part_name[16];
+}Particion;
+
+typedef struct MasterBootRecord{
+    int mbr_tamanio;
+    time_t mbr_fecha_creacion;
+    int mbr_disk_signature;
+    Particion mbr_particion[4];
+}MBR;
+
+typedef struct ExtendenBootRecord{
+    char part_status;
+    char part_fit;
+    int part_start;
+    int part_size;
+    int part_next;
+    //int part_previous;
+    char part_name[16];
+}EBR;
+
+#endif // STRUCTS_H
